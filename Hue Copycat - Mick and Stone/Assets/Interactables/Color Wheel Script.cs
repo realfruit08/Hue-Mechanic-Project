40d47e8bc128f9e7579921de82ea0e2aa790cd42
stonefruit1 <stonebreedon@icloud.com>
2025-02-11 12:04:19 -0500

    updated script and implementation

--- Hue Copycat - Mick and Stone/Assets/Interactables/Color Wheel Script.cs
+++ Hue Copycat - Mick and Stone/Assets/Interactables/Color Wheel Script.cs
@@ -5,6 +5,30 @@ public class ColorWheelScript : MonoBehaviour
 
     public GameObject colorWheel;
 
+    public GameObject PinkBoxes;
+    public GameObject YellowBoxes;
+    public GameObject BlueBoxes;
+    public void Blue()
+    {
+        PinkBoxes.SetActive(true);
+        YellowBoxes.SetActive(true);
+        BlueBoxes.SetActive(false);
+    } 
+
+    public void Yellow()
+    {
+        PinkBoxes.SetActive(true);
+        YellowBoxes.SetActive(false);
+        BlueBoxes.SetActive(true);
+    }
+
+    public void Pink()
+    {
+        PinkBoxes.SetActive(false);
+        YellowBoxes.SetActive(true);
+        BlueBoxes.SetActive(true);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
