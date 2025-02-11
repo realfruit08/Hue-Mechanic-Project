1cef235b9bcc39aeaf36657112a3b81b9a6baff4
realfruit08 <mick.mcnulty08@gmail.com>
2025-02-11 11:45:32 -0500

    color weel

--- Hue Copycat - Mick and Stone/Assets/Interactables/Color Wheel Script.cs
+++ Hue Copycat - Mick and Stone/Assets/Interactables/Color Wheel Script.cs
@@ -4,24 +4,23 @@ public class ColorWheelScript : MonoBehaviour
 {
 
     public GameObject colorWheel;
-    public Camera maincamera;
-
-    bool isWheelActive = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        colorWheel.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-    {
-        colorWheel.SetActive(true);
-        Time.timeScale = 0.3f;
-           
-    }
+        if (Input.GetMouseButton(0))
+        {
+            
+            Debug.Log("colorwheel?");
+            colorWheel.SetActive(true);
+            Time.timeScale = 0.3f;
+        }
+
     }
 }
